8f37f093c1d6118368caeed07af5a7cfcf0d1d0c
AldoIrvine111 <aldoirvine@gmail.com>
2020-10-06 23:13:58 +1100

    Assigned worker now have select option for service

--- FrontEnd/agme/src/pages/modals/assignWorker.js
+++ FrontEnd/agme/src/pages/modals/assignWorker.js
@@ -32,20 +32,101 @@ class AssignWorker extends React.Component {
         if (typeof resp.error != "undefined") {
             this.showErrorModal(resp.error);
         } else {
-            this.showServiceCreated("Service is created");
+            this.showServiceCreated("Service is Deleted");
             window.location = "admin-dashboard"
         }
+
+    }
+
+    getServices() {
+
+        const data = encodeURI('auth-token=' + localStorage.getItem('auth_token'));
+
+        fetch(config.APP_URL + 'service/getall', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: data
+        })
+            .then(res => res.json())
+            .then(res => this.populateServices(res))
+
+    }
+
+    populateServices(res) {
+
+        var parsedData = {};
+        var index;
+        var len;
+
+        for (var k of Object.values(res)) {
+            parsedData[k.id] = {
+                "id": k.id,
+                "name": k.name,
+                "type": k.type,
+                "workers": k.workers
+            };
+        }
+
+        window.datas = parsedData;
+
+        this.renderTableData();
+
+    }
+
+    renderTableData() {
+
+        for (var k of Object.values(window.datas)) {
+
+            const { id, type, name, } = k
+
+            if (window.selectedService == null) {
+                window.selectedService = id;
+            }
+
+            document.getElementById('service').innerHTML += "<option id='" + id + "'>" + name + "</option>";
+
+        };
+
+    }
+
+    componentWillMount() {
+        this.getServices();
+    }
+
+    handleService(e) {
+
+        var select = document.getElementById('service');
+
+        window.selectedService = select.options[select.selectedIndex];
+
+
+        for (var k of Object.values(window.datas)) {
+
+            if (k.id == window.selectedService.id) {
+
+                for (var g of Object.values(k.workers)) {
+
+                    if (window.selectedWorker == "") {
+                        window.selectedWorker = g.id;
+                    }
+
+                };
+
+            }
+
+        };
 
     }
 
     handleSubmit(event) {
 
         event.preventDefault();
-        var sid = document.getElementById('service-id').value;
+
         var uid = document.getElementById('user-id').value;
 
-
-        const data = encodeURI('auth-token=' + localStorage.getItem('auth_token') + '&service-id=' + sid + "&user-id=" + uid);
+        const data = encodeURI('auth-token=' + localStorage.getItem('auth_token') + '&service-id=' + window.selectedService.id + '&user-id=' + uid);
 
         fetch(config.APP_URL + 'service/assignworker', {
             method: 'POST',
@@ -59,20 +140,22 @@ class AssignWorker extends React.Component {
 
     }
 
+
+
     render() {
 
         return (
 
-            
             <Form className="login-form" onSubmit={this.handleSubmit}>
                 <h1 className="font-weight-bold" id="heading">Assign Worker</h1>
                 <div className="alert alert-danger d-none" id="errorMessage">
                 </div>
                 <div className="alert alert-success d-none" id="successMessage"></div>
-
                 <FormGroup>
-                    <Label>Service ID</Label>
-                    <Input type="text" id="service-id" name="service-id" placeholder="Service ID" ref={node => (this.serviceid = node)}></Input>
+                    <Label for="service">Services</Label>
+                    <Input type="select" name="service" id="service" onChange={this.handleService}>
+                        <option value=""></option>
+                    </Input>
                 </FormGroup>
 
                 <FormGroup>
@@ -80,11 +163,9 @@ class AssignWorker extends React.Component {
                     <Input type="text" id="user-id" name="user-id" placeholder="User ID" ref={node => (this.userid = node)}></Input>
                 </FormGroup>
 
-                <Button className="btn-lg btn-success btn-block mt-4 mb-3">
-                    Add Service
-                 </Button>
-
-
+                <Button className="btn-lg btn-success btn-block mt-5 mb-3" type="submit">
+                    Assign
+                </Button>
             </Form>
         );
 
